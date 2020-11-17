ebb1405613d70ff765d8c523a0bb092a2d3a88d0
lfp1024 <lfp1024@126.com>
2020-11-17 22:29:20 +0800

    log

--- app/extend/context.js
+++ app/extend/context.js
@@ -5,12 +5,16 @@ const TRACER = Symbol('tracer');
 module.exports = {
   get tracer() {
     if (!this[TRACER]) {
+      console.log('extend tracer1')
       this[TRACER] = new this.app.config.tracer.Class(this);
     }
+    console.log('extend tracer2')
     return this[TRACER];
   },
 
   get traceId() {
+    console.log('extend tracer3')
+
     return this.tracer.traceId;
   },
 };
